c7c9efa38d3e9e7a160b5581e674d0883b554f16
Eduardo Henrique <edu.hen.fm@gmail.com>
2022-08-26 11:09:44 -0400

    Added support to MangaHost (Partial)
    Signed-off-by: Eduardo Henrique <edu.hen.fm@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,30 @@
 const brmangas_scraper = require('./brmangas_scraper');
+const mangahost_scraper = require('./mangahost_scraper');
 
-brmangas_scraper();
+let tags = {
+    'language': 'pt', // It may be en as well
+    'media': 'manga', // It may be anime, novel or webtoon as well
+}
+
+if (tags.language === 'pt') {
+    if (tags.media === 'manga') {
+        brmangas_scraper();
+        mangahost_scraper();
+    } else if (tags.media === 'anime') {
+
+    } else if (tags.media === 'novel') {
+
+    } else if (tags.media === 'webtoon') {
+
+    }
+} else if (tags.language === 'en') {
+    if (tags.media === 'manga') {
+
+    } else if (tags.media === 'anime') {
+
+    } else if (tags.media === 'novel') {
+
+    } else if (tags.media === 'webtoon') {
+
+    }
+}
--- backend/mangahost_scraper.js
+++ backend/mangahost_scraper.js
@@ -1,32 +1,32 @@
 // DOES NOT REQUIRE PUPPETEER
 
-module.exports = brmangas_scraper;
+module.exports = mangahost_scraper;
 
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const url_domain = 'https://www.brmangas.net/lista-de-manga/page/';
+const url_domain = 'https://mangahosted.com/mangas/page/';
 
 const session = axios.create({
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
 });
 
-async function brmangas_scraper() {
+async function mangahost_scraper() {
     try {
         let manga = [];
         const tmp = cheerio.load(await session.get(url_domain + '1/').then(response => response.data));
-        const max_page_count = parseInt(tmp('.page-numbers').eq(-2).text());
+        const max_page_count = parseInt(tmp('.last').attr('href').split('/').pop());
         
-        console.log('[BRMANGAS] Getting Manga...');
+        console.log('[MANGAHOST] Getting Manga...');
         
         for (let i = 1; i < max_page_count; i++) {
             let tmp2 = cheerio.load(await session.get(url_domain + i).then(response => response.data));
             
-            manga = await manga.concat(tmp2('.item').map((i, el) => {
-                const title = tmp2(el).children().attr('title');
-                const href = tmp2(el).children().attr('href');
-                const img = tmp2(el).children().find('.img-responsive').attr('original-src');
+            manga = await manga.concat(tmp2('.w-col-3').map((i, el) => {
+                const title = tmp2(el).children().find('.manga-block-title-link').attr('title');
+                const href = tmp2(el).children().find('.manga-block-title-link').attr('href');
+                const img = tmp2(el).children().find('.manga-block-img-img').attr('src');
                 
                 return {
                     'title': title,
@@ -36,9 +36,11 @@ async function brmangas_scraper() {
             }).get());
         }
         
-        console.log('[BRMANGAS] Getting Manga DONE.');
+        console.log('[MANGAHOST] Getting Manga DONE.');
     } catch (error) {
         console.error(error);
         throw error;
     }
 }
+
+mangahost_scraper();
