d3ec16b377e6fe6d67f2be664a61e4bdfbd51334
Rosialdo <79479462+Rosialdo@users.noreply.github.com>
2022-09-08 13:33:55 -0300

    Change colors and readjustment the margin of app

--- frontend/src/components/SelectionBar/SelectionBar.js
+++ frontend/src/components/SelectionBar/SelectionBar.js
@@ -1,14 +1,15 @@
 import './SelectionBar.css';
+import React, { useState } from 'react';
 
 function SelectionBar() {
   return (
     <div className="SelectionBar">
         <div className="section1">
-            Selection
+          Select type
         </div>
 
         <div className="section2">
-            Bar
+          Lgs
         </div>
         
     </div>
