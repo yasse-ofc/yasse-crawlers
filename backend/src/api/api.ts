d49a95f99a262894624931094e3110235d3b9994
Eduardo Henrique <edu.hen.fm@gmail.com>
2022-10-08 07:42:38 -0400

    Updated api.ts to fit new code standards
    Signed-off-by: Eduardo Henrique <edu.hen.fm@gmail.com>

--- backend/src/api/api.ts
+++ backend/src/api/api.ts
@@ -2,16 +2,16 @@ import express from 'express';
 import { searchDB } from '../db/db';
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use( express.json() );
+app.use( express.urlencoded( { extended: true } ) );
 
-app.post('/', async (req, res) => {
+app.post( '/', async (req, res) => {
     const data = req.body;
-    res.send(await searchDB(data.search));
+    res.send( await searchDB( data.search ) );
 });
 
-app.listen(process.env.PORT || port, () => {
-    console.log(`Example app listening on port ${process.env.PORT || port}`);
+app.listen( process.env.PORT || PORT, () => {
+    console.log( `Example app listening on port ${ process.env.PORT || PORT }` );
 });
