84ecd2f10c9b253e2d368b24a51c2105824c74c5
Eduardo Henrique <edu.hen.fm@gmail.com>
2022-10-08 07:58:59 -0400

    Updated 9anime_crawler to fit new code standards
    Signed-off-by: Eduardo Henrique <edu.hen.fm@gmail.com>

--- backend/src/crawlers/anime/9anime_crawler/src/routes.ts
+++ backend/src/crawlers/anime/9anime_crawler/src/routes.ts
@@ -2,30 +2,47 @@ import { Dataset, createCheerioRouter } from 'crawlee';
 
 export const router = createCheerioRouter();
 
-router.addDefaultHandler(async ({ $, request, enqueueLinks }) => {
-    const current_page = parseInt(request.url.split('=')[1]);
-    const last_page = parseInt($('.btn.btn-sm.btn-blank').eq(1).text().slice(3));
+router.addDefaultHandler( async ( { $, request, enqueueLinks } ) => {
+    const currentPage = parseInt( request.url.split( '=' )[ 1 ] );
+    const lastPage = parseInt( $( '.btn.btn-sm.btn-blank' )
+        .eq( 1 )
+        .text()
+        .slice( 3 ));
 
-    if ( current_page <= last_page ) {
+    if ( currentPage <= lastPage ) {
         await enqueueLinks({
-            globs: ['https://9anime.vc/az-list/?page=*'],
+            globs: [ 'https://9anime.vc/az-list/?page=*' ],
         });
     }
 
-    $('.anime-block-ul > .ulclear').first().children().map(async (_i, el) => {
-        const title = $(el).find('.dynamic-name').text().toLowerCase();
-        const href = 'https://9anime.vc' + $(el).find('.dynamic-name').attr('href');
-        const img = $(el).find('.film-poster-img').attr('data-src');
-        const latest_chapter = $(el).find('.fdi-duration').text().split('/')[0].slice(41);
-        const source = '9animes';
-        
-        await Dataset.pushData({
-            title,
-            href,
-            img,
-            latest_chapter,
-            source,
+    $( '.anime-block-ul > .ulclear' )
+        .first()
+        .children()
+        .map(async ( _i, series ) => {
+            const title = $( series )
+                .find( '.dynamic-name' )
+                .text()
+                .toLowerCase();
+            const href = 'https://9anime.vc' + 
+                $( series )
+                    .find('.dynamic-name')
+                    .attr('href');
+            const img = $( series )
+                .find( '.film-poster-img' )
+                .attr( 'data-src' );
+            const latestChapter = $( series )
+                .find( '.fdi-duration' )
+                .text()
+                .split( '/' )[ 0 ]
+                .slice( 41 );
+            const source = '9animes';
+            
+            await Dataset.pushData({
+                title,
+                href,
+                img,
+                latestChapter,
+                source,
+            });
         });
-    });
 });
-
