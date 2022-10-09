502bd3cd982449f5c36e5051a08f95c3579730c0
Eduardo Henrique <edu.hen.fm@gmail.com>
2022-10-08 22:43:50 -0400

    Started working on anitube_crawler
    Signed-off-by: Eduardo Henrique <edu.hen.fm@gmail.com>

--- backend/src/crawlers/cheerio_crawlers/src/anitube_crawler/src/routes.ts
+++ backend/src/crawlers/cheerio_crawlers/src/anitube_crawler/src/routes.ts
@@ -5,25 +5,25 @@ export const router = createCheerioRouter();
 
 router.addDefaultHandler( async ( { enqueueLinks } ) => {
     await enqueueLinks({
-        selector: 'li.card > a',
+        selector: 'div.aniItem > a',
         label: 'series_page'
     });
-    
+
     await enqueueLinks({
         globs: [
-            'https://www.anime-planet.com/anime/watch-online/alpha?page=*',
+            'https://www.anitube.site/lista-de-animes-online/page/*/'
         ],
     });
 });
 
 router.addHandler( 'series_page', async ( { $, request } ) => {
-    const title = $( '#siteContainer > h1' ).text().toLowerCase();
+    const title = $( '.mwidth > h1' ).text().slice( 0, -21 ).toLowerCase();
     const href = request.url;
-    const img = $( '.screenshots' ).eq( 1 ).attr( 'src' );
-    const latestChapter = $( '.card.landscape > a' ).eq( -1 ).text().split( ' ' ).pop();
-    const source = 'animeplanet';
+    const img = $( '#capaAnime > img' ).attr( 'src' );
+    const latestChapter = $( '.pagAniListaContainer > a' ).eq( -1 ).text().match(/(\d+)$/);
+    const source = 'anitube';
 
-    await insertOneToDB('anime', {
+    await insertOneToDB( 'test', {
         title,
         href,
         img,
