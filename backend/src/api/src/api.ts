7d67959c683e7942e97c7f50a7eb02a60032445b
Eduardo Henrique <edu.hen.fm@gmail.com>
2022-10-11 09:53:49 -0400

    Updated all tsconfig.json files
    Signed-off-by: Eduardo Henrique <edu.hen.fm@gmail.com>

--- backend/src/api/src/api.ts
+++ backend/src/api/src/api.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { searchDB } from '../db/src/db.js';
+import { searchDB } from '../../db/src/db.js';
 
 const app = express();
 const PORT = 3000;
@@ -7,7 +7,7 @@ const PORT = 3000;
 app.use( express.json() );
 app.use( express.urlencoded( { extended: true } ) );
 
-app.post( '/', async (req, res) => {
+app.get( '/', async (req, res) => {
     const data = req.body;
     res.send( await searchDB( data.searchTerm ?? 'one piece', data.collectionToSearch ?? 'manga' ) );
 });
